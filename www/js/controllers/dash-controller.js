baa3a74008dbb74a335d49ef6f8560ef9a8f1475
Stephanie Adams <adamsstephaniem@gmail.com>
2016-08-06 16:39:20 -0400

    Create seed file for bus routes

--- www/js/controllers/dash-controller.js
+++ www/js/controllers/dash-controller.js
@@ -1,9 +1,11 @@
 app.controller('DashCtrl', function($scope,DashFactory,$http) {
+  $scope.busRoutes = initialRouteSeeder();
   $scope.greet = 'Welcome!!';
   $scope.alarmingTime = 10;
   // $scope.nextEta = $scope.alarmingTime +1;
+  $scope.route = 190;  $scope.stop = 27270;
   $scope.getNJTdata = function() {
-    return DashFactory.getNJTdata(190,27175)
+    return DashFactory.getNJTdata($scope.route,$scope.stop)
     .then(function(results){
       $scope.etas = results;
       $scope.nextEta = results.reduce(function(prior,curr){
@@ -17,7 +19,7 @@ app.controller('DashCtrl', function($scope,DashFactory,$http) {
     .catch(function(err){console.log(JSON.stringify(err));});
   };
 
-  var url = '/apiNJT/getStopPredictionsETA.jsp?route=190&stop=26804';
   $scope.getNJTdata();
 
+// var url = '/apiNJT/getStopPredictionsETA.jsp?route=190&stop=26804';
 })
