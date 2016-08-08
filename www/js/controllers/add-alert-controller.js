d879b2ebc464eb1cdf5143844363b4295cef9032
Stephanie Adams <adamsstephaniem@gmail.com>
2016-08-08 00:01:04 -0400

    initial alerts view files

--- www/js/controllers/add-alert-controller.js
+++ www/js/controllers/add-alert-controller.js
@@ -1,10 +1,10 @@
-app.controller('AddAlertCtrl', function($scope, Alert) {
+app.controller('AddAlertCtrl', function($scope, Alert, moment) {
   // To listen for when this page is active (to refresh data instead of cache),
   // listen for the $ionicView.enter event:
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-
+  $scope.time=moment();
   $scope.alerts = Alert.all();
   $scope.remove = function(Alert) {
     Alert.remove(Alert);
